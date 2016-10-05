cb5d7d98f7beb0284ba5fdc820fe2c6c3d070684
Jacob Malcom <jmalcom@defenders.org>
2016-10-05 13:51:32 -0400

    Remove a random string.

--- R/query_parse.R
+++ R/query_parse.R
@@ -11,39 +11,3 @@ build_query <- function(q) {
   }
 }
 
-q33 <- '{
-  "query": {
-    "bool": {
-      "should": {
-        "match": {
-          "raw_txt": "recovery unit"
-        }
-      },
-      "must": {
-        "match": {
-          "raw_txt.shingles": "\\\"recovery unit\\\""
-        }
-      },
-      "filter": {
-        "range": {
-          "date": {
-            "gte": "2005-01-01",
-            "lte": "2016-07-07"
-          }
-        }
-      }
-    }
-  },
-  "size": "500",
-  "min_score": 0.1,
-  "highlight": {
-    "fields": {
-      "raw_txt.shingles": {
-        "type": "fvh",
-        "fragment_size": "150",
-        "pre_tags": ["<b>"],
-        "post_tags": ["</b>"]
-      }
-    }
-  }
-}'
